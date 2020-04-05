8cafabd5bfa867d2636b0c94af570ddc9d5d7cc3
unknown <pfdzm@users.noreply.github.com>
2020-04-05 12:37:20 +0200

    update about me

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -1,42 +1,58 @@
 import React from "react"
-import styled from "@emotion/styled"
 
 export default () => (
-  <div>
+  <div className="about-me">
     <h2>About me</h2>
-    <P>
-      Hi there! I'm Pablo and I am learning to be a web developer. I come from
-      Spain, but moved around when I was a kid, including Brussels, Madrid and
-      Berlin. I finished school in Madrid and decided to move to Berlin to study
-      civil engineering.
-    </P>
-    <P>
-      Fast-forward ten years: I have a BSc in civil engineering but work as an
-      admissions manager at ESMT Berlin, an international business school.
-      Having always had a hunger for tech and a deep curiosity for how things
-      work, I begin looking for a way to shift my career into something
-      technical. The Coding Bootcamp seems like a perfect match. And here I am -
-      two months into the bootcamp and enjoying every day of it. As I progress
-      through the program, you too should be able to see my progress reflected
-      on this portfolio page. If you want to reach me, you can do so on{" "}
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://github.com/pfdzm"
-      >
-        GitHub
-      </a>{" "}
-      or{" "}
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://www.linkedin.com/in/pablofdezm/"
-      >
-        LinkedIn
-      </a>
-      .
-    </P>
+    <div>
+      <p>
+        I'm Pablo and I am learning web development. I come from Spain, but
+        moved around quite a bit when I was a kid, having lived for a few years
+        in Germany, Belgium and Spain. Once I did my Abitur at the German School
+        in Madrid, I moved to Berlin to study Civil Engineering at the Berlin
+        Institute of Technology.
+      </p>
+      <p>
+        Fast-forward ten years: I work as an Admissions Manager for the Degree
+        Programs team at ESMT Berlin, an international business school ranked
+        amongst the top 10 European Business Schools. My main work consists of
+        advising, screening and interviewing candidates for the Degree Programs
+        offered at ESMT Berlin.
+      </p>
+      <p>
+        Having always had a hunger for tech and a deep curiosity for how things
+        work, I begin looking for a way to shift my career into something
+        technical. I enrolled in the 24-week fullstack part-time coding bootcamp
+        offered by Trilogy and completed it, devoting most of my evenings and
+        weekends to learning about modern web development. I am now looking for
+        any opportunity to get started in the WebDev field!
+      </p>
+      <p>
+        I am passionate about learning and technology. I love tinkering with
+        things (both in the virtual and real world) and am no stranger to the
+        command line, as well as different OS environments (I grew up using
+        MacOS, Windows and Ubuntu). In real life, I like to play the bass
+        (inlcuding tinkering with the tuning, action, pickups), ride my bike,
+        play videogames and am an avid sci-fi reader.
+      </p>
+      <p>
+        If you want to reach me, you can do so on{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/pfdzm"
+        >
+          GitHub
+        </a>{" "}
+        or{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/pablofdezm/"
+        >
+          LinkedIn
+        </a>
+        . Scroll a bit further down to see my most recent projects!
+      </p>
+    </div>
   </div>
 )
-
-const P = styled.p``
