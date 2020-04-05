fc02b6f6e412828c0ceeb7d2ff74edc73d80ca54
unknown <pfdzm@users.noreply.github.com>
2020-04-05 12:36:17 +0200

    add more projects, screenshot components

--- src/components/portfolioCard.js
+++ src/components/portfolioCard.js
@@ -1,59 +1,36 @@
 import React from "react"
 import styled from "@emotion/styled"
-import Cooknbook from "./screenshots/screenshotCooknbook"
-import { css } from "@emotion/core"
 
-export default ({ title, description, img, repo, deploy }) => (
-  <Wrapper>
-    <div className="card">
-      <div
-        css={css`
-          margin-bottom: 1.0875rem;
-        `}
-      >
-        {title === "cooknbook" && <Cooknbook />}
-      </div>
-      <div className="card-body">
-        <h3 className="card-title">{title}</h3>
-        <p className="card-text">{description}</p>
-        <p className="card-text">
-          <a href={deploy}>Live demo!</a>
-        </p>
-        <p className="card-text">
-          <a href={repo}>Source code</a>
-        </p>
-      </div>
+import Cooknbook from "./screenshots/cooknbook"
+import Doenate from "./screenshots/doenate"
+import WordGuessGame from "./screenshots/wordGuessGame"
+import CodeQuiz from "./screenshots/codeQuiz"
+import WeatherDashboard from "./screenshots/weatherDashboard"
+import PfdzmHome from "./screenshots/pfdzmHome"
+import WorkoutTracker from "./screenshots/workoutTracker"
+import EatDaBurger from "./screenshots/eatDaBurger"
+
+export default ({ title, description, repo, deploy }) => (
+  <div className="card">
+    <div className="card-img">
+      {title === "CookNBook" && <Cooknbook />}
+      {title === "Dönate" && <Doenate />}
+      {title === "Word Guess Game" && <WordGuessGame />}
+      {title === "CodeQuiz" && <CodeQuiz />}
+      {title === "Weather Dashboard" && <WeatherDashboard />}
+      {title === "pfdzm Home" && <PfdzmHome />}
+      {title === "Workout Tracker" && <WorkoutTracker />}
+      {title === "Eat-da-Burger" && <EatDaBurger />}
     </div>
-  </Wrapper>
+    <div className="card-body">
+      <h3 className="card-title">{title}</h3>
+      <p className="card-text">{description}</p>
+      <p className="card-text">
+        <a href={deploy}>Live demo!</a>
+      </p>
+      <p className="card-text">
+        <a href={repo}>Source code</a>
+      </p>
+    </div>
+  </div>
 )
-
-const Wrapper = styled.div`
-  .card {
-    max-width: 18rem;
-    display: inline-block;
-    border: 1px solid #eee;
-    box-shadow: 0px 0px 5px #eee;
-    border-radius: 5px;
-    padding: 1.4rem 1.8rem;
-    margin-bottom: 2rem;
-  }
-
-  .card img {
-    max-width: 100%;
-    height: auto;
-    border: 1px solid #eee;
-    border-radius: 5px;
-    margin-bottom: 1.0875rem;
-  }
-
-  .card-title {
-    font-size: inherit;
-    margin-top: 0.1rem;
-    margin-bottom: 0.2rem;
-  }
-
-  .card-text {
-    margin: 0;
-    padding: 0;
-  }
-`
--- src/data/index.js
+++ src/data/index.js
@@ -1,6 +1,28 @@
 export const projects = [
   {
-    title: "cooknbook",
+    title: "Dönate",
+    img: {
+      url: "/assets/images/donate.png",
+      alt: "doenate screenshot",
+    },
+    description:
+      "Help Berlin, Dönate a Döner! Simple web app built with React (incl. Hooks, Context API and reducers to handle state) and Stripe Checkout API to funnel donations to local Berlin charities. Offers one-time or subscription payments.",
+    repo: "https://github.com/pfdzm/doenate.git",
+    deploy: "https://doner.now.sh/",
+  },
+  {
+    title: "pfdzm Home",
+    img: {
+      url: "",
+      alt: "homepage screenshot",
+    },
+    description:
+      "My own homepage, displaying a bit of info about myself, as well as a collection of my most recent projects. Built with Gatsby (+ React), deployed on Netlify.",
+    repo: "https://github.com/pfdzm/pfdzm-home.git",
+    deploy: "#",
+  },
+  {
+    title: "CookNBook",
     img: {
       url: "/assets/images/cooknbook.png",
       alt: "cooknbook screenshot",
@@ -15,23 +37,59 @@ export const projects = [
     deploy: "https://dnedaniele.github.io/cooknbook/",
   },
   {
-    title: "Dönate",
+    title: "Word Guess Game",
     img: {
-      url: "/assets/images/donate.png",
-      alt: "doenate screenshot",
+      url: "",
+      alt: "word-guess-game screenshot",
     },
-    description: "",
-    repo: "https://github.com/pfdzm/doenate.git",
-    deploy: "https://doner.now.sh/",
+    description:
+      "A little hangman-like game built with vanilla JS, HTML and CSS. Hacker theme.",
+    repo: "https://github.com/pfdzm/Word-Guess-Game.git",
+    deploy: "https://pfdzm.github.io/Word-Guess-Game",
   },
   {
-    title: "cooknbook",
+    title: "CodeQuiz",
     img: {
-      url: "/assets/images/cooknbook.png",
-      alt: "cooknbook screenshot",
+      url: "",
+      alt: "code quiz screenshot",
     },
-    description: "",
-    repo: "https://github.com/dnedaniele/cooknbook.git",
-    deploy: "https://dnedaniele.github.io/cooknbook/",
+    description:
+      "A simple quiz app to test your coding chops! Includes an easy and a 'hard' mode. Built with vanilla JS, HTML and CSS (Bootstap).",
+    repo: "https://github.com/pfdzm/Code-Quiz.git",
+    deploy: "https://pfdzm.github.io/Code-Quiz/",
+  },
+  {
+    title: "Weather Dashboard",
+    img: {
+      url: "",
+      alt: "weather dashboard screenshot",
+    },
+    description:
+      "A weather dashboard built using vanilla JS, HTML and CSS (Bootstrap). Uses the Open Weather API to query for data, displays on a LeafletJS map using Mapbox tiles.",
+    repo: "https://github.com/pfdzm/Weather-Dashboard.git",
+    deploy: "https://pfdzm.github.io/Weather-Dashboard/",
+  },
+
+  {
+    title: "Eat-da-Burger",
+    img: {
+      url: "",
+      alt: "eat-da-burger screenshot",
+    },
+    description:
+      "A very simple app to utilize the full stack: backend in NodeJS, Server in Express, Persistence/Models in MySQL, Handlebars for the frontend.",
+    repo: "https://github.com/pfdzm/burger.git",
+    deploy: "https://pfdzm-eat-da-burger.herokuapp.com/",
+  },
+  {
+    title: "Workout Tracker",
+    img: {
+      url: "",
+      alt: "workout tracker screenshot",
+    },
+    description:
+      "A full-stack workout tracker deployed on Heroku. Uses Mongoose + MongoDB for persistence.",
+    repo: "https://github.com/pfdzm/workout-tracker.git",
+    deploy: "https://pfdzm-workout-tracker.herokuapp.com/",
   },
 ]
