1873fe5d0ed43e664345b4e5c68084983f28606e
unknown <pfdzm@users.noreply.github.com>
2020-04-05 12:37:51 +0200

    remove unused imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,27 +5,66 @@ import SEO from "../components/seo"
 import PortfolioCard from "../components/portfolioCard"
 import AboutMe from "../components/aboutMe"
 import { projects } from "../data"
+import styled from "@emotion/styled"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <AboutMe />
-    <div
-      className="projectContainer"
-      style={{
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: "space-between",
-      }}
-    >
-      {projects.map(proj => (
-        <PortfolioCard
-          {...proj}
-          key={proj.title + proj.deploy.length + Math.random().toString()}
-        />
-      ))}
+    <div className="projects">
+      <h2>My projects</h2>
+      <Wrapper>
+        {projects.map(proj => (
+          <PortfolioCard
+            {...proj}
+            key={proj.title + proj.deploy.length + Math.random().toString()}
+          />
+        ))}
+      </Wrapper>
     </div>
   </Layout>
 )
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: start;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  .card {
+    max-width: 300px;
+    display: inline-block;
+    border: 1px solid #eee;
+    box-shadow: 0px 0px 2px #eee;
+    border-radius: 5px;
+    padding: 1.0875rem 1.63125rem;
+    margin-bottom: 2rem;
+  }
+
+  .card-img {
+    border: 1px solid #eee;
+    box-shadow: 0px 0px 2px #eee;
+    border-radius: 5px;
+    margin-bottom: 1.0875rem;
+  }
+
+  .card img {
+    border-radius: 5px;
+  }
+
+  .card-title {
+    font-size: larger;
+    margin-top: 0.1rem;
+    margin-bottom: 1.0875rem;
+  }
+
+  .card-text {
+    font-size: inherit;
+    margin-bottom: 1.0875rem;
+  }
+
+  .card-text:last-child {
+    margin-bottom: 0;
+  }
+`
 
 export default IndexPage
