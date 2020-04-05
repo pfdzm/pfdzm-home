6fa12016051ecbca8907966a5c2cdde35c5bdb9c
Pablo Fernandez <pfdzm@users.noreply.github.com>
2020-04-05 12:51:13 +0200

    update favicon, remove unused boilerplate

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `pfdzm's personal page`,
+        short_name: `pfdzm`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `white`,
+        theme_color: `palevioletred`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/me.jpg`, // This path is relative to the root of the site.
       },
     },
     // add emotion.js plugin
